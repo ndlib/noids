8352ebaf1c76020ba8f032ff74171a5fdda0f3b7
Don Brower <dbrower@nd.edu>
2014-05-06 15:52:22 -0400

    Add support for config file. Also, trap all the signals.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -10,10 +11,11 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/dbrower/noids/server"
-
+	"code.google.com/p/gcfg"
 	_ "code.google.com/p/go-sqlite/go1/sqlite3"
 	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/dbrower/noids/server"
 )
 
 type Reopener interface {
@@ -49,11 +51,27 @@ func (li *loginfo) Reopen() {
 
 func signalHandler(sig <-chan os.Signal, logw Reopener) {
 	for s := range sig {
-		log.Println("Got", s)
+		log.Println("Received signal", s)
 		switch s {
 		case syscall.SIGUSR1:
 			logw.Reopen()
+		case syscall.SIGINT, syscall.SIGTERM:
+			log.Println("Exiting")
+			os.Exit(1)
 		}
+	}
+}
+
+type Config struct {
+	General struct {
+		Port string
+	}
+	Mysql struct {
+		User     string
+		Password string
+		Host     string
+		Port     string
+		Database string
 	}
 }
 
@@ -65,6 +83,9 @@ func main() {
 		logw          Reopener
 		sqliteFile    string
 		mysqlLocation string
+		showVersion   bool
+		configFile    string
+		config        Config
 	)
 
 	flag.StringVar(&port, "port", "13001", "port to run on")
@@ -72,18 +93,44 @@ func main() {
 	flag.StringVar(&storageDir, "storage", "", "directory to save noid information")
 	flag.StringVar(&sqliteFile, "sqlite", "", "sqlite database file to save noid information")
 	flag.StringVar(&mysqlLocation, "mysql", "", "MySQL database to save noid information")
+	flag.BoolVar(&showVersion, "version", false, "Display binary version")
+	flag.StringVar(&configFile, "config", "", "config file to use")
 
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("noids version %s\n", version)
+		return
+	}
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 	logw = NewReopener(logfilename)
 	logw.Reopen()
 	log.Println("-----Starting Server")
-	log.Println(" Port:", port)
-	log.Println(" Log:", logfilename)
+
+	if configFile != "" {
+		log.Printf("Reading config file %s", configFile)
+		err := gcfg.ReadFileInto(&config, configFile)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			// config file overrides command line
+			if config.General.Port != "" {
+				port = config.General.Port
+			}
+			if config.Mysql.Database != "" {
+				mysqlLocation = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+					config.Mysql.User,
+					config.Mysql.Password,
+					config.Mysql.Host,
+					config.Mysql.Port,
+					config.Mysql.Database)
+			}
+		}
+	}
 
 	sig := make(chan os.Signal, 5)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGUSR1, syscall.SIGUSR2)
+	signal.Notify(sig)
 	go signalHandler(sig, logw)
 
 	var (
@@ -109,6 +156,7 @@ func main() {
 		saver = server.NewDbFileSaver(db)
 	}
 	server.SetupHandlers(saver)
+	log.Println("Listening on port", port)
 	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
